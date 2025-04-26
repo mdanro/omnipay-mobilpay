b49798944c7eae1838faccbd319f95236b046739
danielmarin <mdanro@gmail.com>
2025-04-27 00:33:21 +0300

    open ssl card patch

--- src/BusinessMastery/Mobilpay/Api/Request/Card.php
+++ src/BusinessMastery/Mobilpay/Api/Request/Card.php
@@ -52,83 +52,87 @@ class Card extends AbstractRequest
         return $this;
     }
 
-    protected function _prepare()
-    {
-        if (is_null($this->signature) || is_null($this->orderId) || !($this->invoice instanceof Invoice)) {
-            throw new Exception('One or more mandatory properties are invalid!', self::ERROR_PREPARE_MANDATORY_PROPERTIES_UNSET);
-        }
+ protected function _prepare()
+	{
+		if(is_null($this->signature) || is_null($this->orderId) || !($this->invoice instanceof Mobilpay_Payment_Invoice))
+		{
+			throw new Exception('One or more mandatory properties are invalid!', self::ERROR_PREPARE_MANDATORY_PROPERTIES_UNSET);
+		}
+		
+		$this->_xmlDoc 		= new DOMDocument('1.0', 'utf-8');
+		$rootElem 			= $this->_xmlDoc->createElement('order');
 
-        $this->_xmlDoc        = new DOMDocument('1.0', 'utf-8');
-        $rootElem            = $this->_xmlDoc->createElement('order');
+		//set payment type attribute
+		$xmlAttr 			= $this->_xmlDoc->createAttribute('type');
+		$xmlAttr->nodeValue	= $this->type;
+		$rootElem->appendChild($xmlAttr);
+		
+		//set id attribute
+		$xmlAttr 			= $this->_xmlDoc->createAttribute('id');
+		$xmlAttr->nodeValue	= $this->orderId;
+		$rootElem->appendChild($xmlAttr);
+		
+		//set timestamp attribute
+		$xmlAttr 			= $this->_xmlDoc->createAttribute('timestamp');
+		$xmlAttr->nodeValue	= date('YmdHis');
+		$rootElem->appendChild($xmlAttr);
+		
+		$xmlElem			= $this->_xmlDoc->createElement('signature');
+		$xmlElem->nodeValue	= $this->signature;
+		$rootElem->appendChild($xmlElem);
+		
+		$xmlElem			= $this->invoice->createXmlElement($this->_xmlDoc);
+		$rootElem->appendChild($xmlElem);
+		if($this->ipnCipher !== null)
+		{
+			$xmlElem = $this->_xmlDoc->createElement('ipn_cipher');
+			$xmlElem->nodeValue = $this->ipnCipher;
+			$rootElem->appendChild($xmlElem);	
+		}
+		
+		if(is_array($this->params) && sizeof($this->params) > 0)
+		{
+			$xmlParams = $this->_xmlDoc->createElement('params');
+			foreach ($this->params as $key=>$value)
+			{
+				$xmlParam 	= $this->_xmlDoc->createElement('param');
+				
+				$xmlName			= $this->_xmlDoc->createElement('name');
+				$xmlName->nodeValue = trim($key);
+				$xmlParam->appendChild($xmlName);
+				
+				$xmlValue			= $this->_xmlDoc->createElement('value');
+				$xmlValue->appendChild($this->_xmlDoc->createCDATASection($value));
+				$xmlParam->appendChild($xmlValue);
+				
+				$xmlParams->appendChild($xmlParam);
+			}
+			
+			$rootElem->appendChild($xmlParams);
+		}
+		
+		if(!is_null($this->returnUrl) || !is_null($this->confirmUrl))
+		{
+			$xmlUrl = $this->_xmlDoc->createElement('url');
+			if(!is_null($this->returnUrl))
+			{
+				$xmlElem = $this->_xmlDoc->createElement('return');
+				$xmlElem->nodeValue = $this->returnUrl;
+				$xmlUrl->appendChild($xmlElem); 
+			}
+			if(!is_null($this->confirmUrl))
+			{
+				$xmlElem = $this->_xmlDoc->createElement('confirm');
+				$xmlElem->nodeValue = $this->confirmUrl;
+				$xmlUrl->appendChild($xmlElem); 
+			}
+			
+			$rootElem->appendChild($xmlUrl);
+		}
+		
+		$this->_xmlDoc->appendChild($rootElem);
 
-        //set payment type attribute
-        $xmlAttr            = $this->_xmlDoc->createAttribute('type');
-        $xmlAttr->nodeValue    = $this->type;
-        $rootElem->appendChild($xmlAttr);
-
-        //set id attribute
-        $xmlAttr            = $this->_xmlDoc->createAttribute('id');
-        $xmlAttr->nodeValue    = $this->orderId;
-        $rootElem->appendChild($xmlAttr);
-
-        //set timestamp attribute
-        $xmlAttr            = $this->_xmlDoc->createAttribute('timestamp');
-        $xmlAttr->nodeValue    = date('YmdHis');
-        $rootElem->appendChild($xmlAttr);
-
-        $xmlElem            = $this->_xmlDoc->createElement('signature');
-        $xmlElem->nodeValue    = $this->signature;
-        $rootElem->appendChild($xmlElem);
-
-        $xmlElem            = $this->_xmlDoc->createElement('service');
-        $xmlElem->nodeValue    = $this->service;
-        $rootElem->appendChild($xmlElem);
-
-        $xmlElem            = $this->invoice->createXmlElement($this->_xmlDoc);
-        $rootElem->appendChild($xmlElem);
-
-        if ($this->recurrence instanceof Recurrence) {
-            $xmlElem            = $this->recurrence->createXmlElement($this->_xmlDoc);
-            $rootElem->appendChild($xmlElem);
-        }
-
-        if (is_array($this->params) && sizeof($this->params) > 0) {
-            $xmlParams = $this->_xmlDoc->createElement('params');
-            foreach ($this->params as $key=>$value) {
-                $xmlParam    = $this->_xmlDoc->createElement('param');
-
-                $xmlName            = $this->_xmlDoc->createElement('name');
-                $xmlName->nodeValue = trim($key);
-                $xmlParam->appendChild($xmlName);
-
-                $xmlValue            = $this->_xmlDoc->createElement('value');
-                $xmlValue->appendChild($this->_xmlDoc->createCDATASection($value));
-                $xmlParam->appendChild($xmlValue);
-
-                $xmlParams->appendChild($xmlParam);
-            }
-
-            $rootElem->appendChild($xmlParams);
-        }
-
-        if (!is_null($this->returnUrl) || !is_null($this->confirmUrl)) {
-            $xmlUrl = $this->_xmlDoc->createElement('url');
-            if (!is_null($this->returnUrl)) {
-                $xmlElem = $this->_xmlDoc->createElement('return');
-                $xmlElem->nodeValue = $this->returnUrl;
-                $xmlUrl->appendChild($xmlElem);
-            }
-            if (!is_null($this->confirmUrl)) {
-                $xmlElem = $this->_xmlDoc->createElement('confirm');
-                $xmlElem->nodeValue = $this->confirmUrl;
-                $xmlUrl->appendChild($xmlElem);
-            }
-
-            $rootElem->appendChild($xmlUrl);
-        }
-
-        $this->_xmlDoc->appendChild($rootElem);
-
-        return $this;
-    }
+		return $this;
+	}
+	
 }
